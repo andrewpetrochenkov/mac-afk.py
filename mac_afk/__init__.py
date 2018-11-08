f539afd0e1b1c32b0a5f900789fa9c8477f7f66e
looking-for-a-job <russianidiot.github@gmail.com>
2018-11-08 19:08:13 +0300

    +classifiers.txt; ^.travis.yml,mac_afk/__init__.py,setup.cfg,tests/mac-afk/ ...

--- mac_afk/__init__.py
+++ mac_afk/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from public import public
+import public
 import runcmd
 
-@public
+
+@public.add
 def seconds():
     return int(runcmd.run(["afk"])._raise().out)
