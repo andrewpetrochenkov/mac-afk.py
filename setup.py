ccddc1face37a7633439b793c8b6d6bb46a9d42d
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:04:27 +0300

    2020:12:04 00:04

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-afk',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/afk']
